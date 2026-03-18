35403fdd67f3b1d83822bafcb7d74608c41f7891
YoungMayor <youngmayor.dev@gmail.com>
2026-03-18 17:53:42 +0100

    Phase 1: Implement Foundations (Syntax Highlighting & Basic Parsing)

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { parsePrompt } from "./parser";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "promptsense" is now active!');
+	console.log('Congratulations, your extension "promptsense" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand(
+	const disposable = vscode.workspace.onDidOpenTextDocument((document) => {
+		if (document.languageId === "promptsense") {
+			const parsed = parsePrompt(document.getText());
+			console.log("Parsed .prompt file:", parsed);
+		}
+	});
+
+	const helloWorld = vscode.commands.registerCommand(
 		"promptsense.helloWorld",
 		() => {
-			// The code you place here will be executed every time your command is executed
-			// Display a message box to the user
 			vscode.window.showInformationMessage("Hello World from PromptSense!");
 		},
 	);
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable, helloWorld);
 }
 
 // This method is called when your extension is deactivated
